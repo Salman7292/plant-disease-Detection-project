71f017fa1e58f03e23e08bcc77f8fb9a1106b923
Salman Malik <162824871+Salman7292@users.noreply.github.com>
2024-08-28 17:14:48 +0500

    Update index.py

--- index.py
+++ index.py
@@ -35,28 +35,7 @@ with open(file_path, 'r') as file:
 
 
 
-# Function to Load and Preprocess the Image using Pillow
-def load_and_preprocess_image(image_path, target_size=(224, 224)):
-    # Load the image
-    img = Image.open(image_path)
-    # Resize the image
-    img = img.resize(target_size)
-    # Convert the image to a numpy array
-    img_array = np.array(img)
-    # Add batch dimension
-    img_array = np.expand_dims(img_array, axis=0)
-    # Scale the image values to [0, 1]
-    img_array = img_array.astype('float32') / 255.
-    return img_array
 
-# Function to Predict the Class of an Image
-def predict_image_class(model, image_path, class_indices):
-    preprocessed_img = load_and_preprocess_image(image_path)
-    predictions = model.predict(preprocessed_img)
-    predicted_class_index = np.argmax(predictions, axis=1)[0]
-    predicted_class_name = class_indices[predicted_class_index]
-
-    return predicted_class_name
 
 
 
